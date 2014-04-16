b093affd72cf68fef8a996f08222c21073ac407a
Jonathan An <jonnyleean@gmail.com>
2014-04-16 16:55:27 -0700

    Ship movement + boundaries
    Better ship movement and still working on  boundaries

--- UnitySpaceProject/Assets/Scripts/ShipBehavior.cs
+++ UnitySpaceProject/Assets/Scripts/ShipBehavior.cs
@@ -2,38 +2,28 @@
 using System.Collections;
 
 public class ShipBehavior : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
-	}
+	public float speed;
+	public float rotationSpeed;
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.x--;
-			this.transform.position = position;
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.x++;
-			this.transform.position = position;
-		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y++;
-			this.transform.position = position;
-		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y--;
-			this.transform.position = position;
-		}
+		// Get the horizontal and vertical axis.
+		// By default they are mapped to the arrow keys.
+		// The value is in the range -1 to 1
+		float translationH = Input.GetAxis ("Horizontal") * speed;
+		float translationV = Input.GetAxis ("Vertical") * speed;
+		float rotationV = Input.GetAxis ("Vertical") * rotationSpeed;
+		float rotationH = Input.GetAxis ("Horizontal") * rotationSpeed;
+		
+		// Make it move 10 meters per second instead of 10 meters per frame...
+		translationH *= Time.deltaTime;
+		translationV *= Time.deltaTime;
+		rotationH *= Time.deltaTime;
+		rotationV *= Time.deltaTime;
 
+		//move ship
+		transform.Translate (0, translationV, translationH);
+		transform.Rotate (rotationH, 0, rotationV);
 	}
 }
