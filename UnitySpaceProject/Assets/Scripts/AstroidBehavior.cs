27eb918a445605ad9540173c22d7714747faa748
Shayan Mehrazarin <smehr003@ucr.edu>
2014-04-21 16:48:15 -0700

    Added Astroid Movement
    Includes use of random number generator when respawning the astroid.

---
+++ UnitySpaceProject/Assets/Scripts/AstroidBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstroidBehavior : MonoBehaviour {
+
+	public float ast_direction = 0.0f;
+	// Use this for initialization
+	void Start () {
+		ast_direction = Random.Range (-1.0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.Translate(Vector3.front * -7 * Time.deltaTime);
+
+		Vector3 pos = transform.position;
+
+		pos.x += 0.05f * ast_direction * Time.deltaTime;
+		pos.y += 0.02f * ast_direction * Time.deltaTime;
+		pos.z += -3f * Time.deltaTime;
+		transform.position = pos;
+
+		if (pos.z <= -15) 
+		{
+			pos.x = 0;
+			pos.y = 0;
+			pos.z = 50;
+
+			transform.position = pos;
+
+			ast_direction = Random.Range (-1.0f, 1.0f);
+		}
+	}
+}
